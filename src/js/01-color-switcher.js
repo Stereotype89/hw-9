a0e3718dd1f6630ce791acaa542edaa4374b84cc
Oleksandr Shyshman <sh.alexandr.1989@gmail.com>
2024-05-31 21:03:45 +0200

    complete first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyColor = document.body.style;
+let changeColorInterval = null;
 
+btnStart.addEventListener('click', () => {
+  btnStart.setAttribute('disabled', '');
+  changeColorInterval = setInterval(() => {
+    bodyColor.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+5;
+btnStop.addEventListener('click', () => {
+  btnStart.removeAttribute('disabled', '');
+  clearInterval(changeColorInterval);
+});
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
